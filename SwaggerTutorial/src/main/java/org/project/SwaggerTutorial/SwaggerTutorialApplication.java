b5d5217bf8f87f53e80d676499d44902ec5664c2
codemaker-kim <tnals72441@daum.net>
2024-10-11 23:41:47 +0900

    #2 - Swagger 기초 환경 설정.
    스웨거에 대한 기초 환경 설정을 해보았다.

---
+++ SwaggerTutorial/src/main/java/org/project/SwaggerTutorial/SwaggerTutorialApplication.java
@@ -0,0 +1,11 @@
+package org.project.SwaggerTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerTutorialApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerTutorialApplication.class, args);
+    }
+}
